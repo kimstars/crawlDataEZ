0d56093d1d514475fc1c780709486d0b169f7802
kimstars <buikika9@gmail.com>
2024-06-18 22:48:00 +0700

    2024 crawl

--- test.py
+++ test.py
@@ -5,30 +5,39 @@ name = []
 toan = []
 van = []
 anh = []
+SBD = []
 
 id = 130000
-for i in range(1,442):
+for i in range(1, 385):
     try:
         id += 1
-        link = "http://tracuudiem.thi.phutho.vn/Home/TraCuu?MaNhom=TS10%202022-2023&SoBD="+ str(id)
+        link = "http://tracuudiem.thi.phutho.vn/Home/TraCuu?MaNhom=TS10%202024-2025&SoBD=" + str(id)
         res = requests.get(link)
         data = res.json()
-        
-        name.append(data[0]['TenMon'])
-        toan.append(data[1]['Diem'])
-        van.append(data[2]['Diem'])
-        anh.append(data[3]['Diem'])
+        print(data)
+        if(len(data) == 4):
+            SBD.append(data[0]['Diem'])
+            name.append(data[0]['TenMon'])
+            van.append(data[1]['Diem'])
+            toan.append(data[2]['Diem'])
+            anh.append(data[3]['Diem'])
 
-        
-        print(id)
-    except:
-        pass
+            print(id)
+        # if (id > 130010) :
+        #     break
+    except Exception as e:
+        print(f"Error at id {id}: {e}")
+        break
 
+
+# Tạo DataFrame và xuất ra file Excel
+col0 = "SBD"
 col1 = "Họ và tên"
 col2 = "Toán"
 col3 = "Văn"
 col4 = "Anh"
 
-data = pd.DataFrame({col1:name,col2:toan,col3:van,col4:anh})
-data.to_excel('data.xlsx', sheet_name='sheet1', index=False)
+print(len(SBD), len(name), len(toan), len(van), len(anh))
 
+data = pd.DataFrame({col0: SBD, col1: name, col2: toan, col3: van, col4: anh})
+data.to_excel('data2025.xlsx', sheet_name='sheet1', index=False)
